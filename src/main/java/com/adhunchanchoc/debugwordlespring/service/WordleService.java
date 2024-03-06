002e2322acffe0144dd9d9daff73e3e7df05959e
adhunchanchoc <adhunchanchoc@gmail.com>
2024-03-06 19:11:33 +0100

    simple REST was converted to pluggable MVC architecture with a single service

---
+++ src/main/java/com/adhunchanchoc/debugwordlespring/service/WordleService.java
@@ -0,0 +1,56 @@
+package com.adhunchanchoc.debugwordlespring.service;
+
+import com.adhunchanchoc.debugwordlespring.webservices.CharacterResult;
+import com.adhunchanchoc.debugwordlespring.webservices.Result;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@Service
+public class WordleService {
+    private static final String WORD = "LYMPH";
+    private static final List<String> DICTIONARY = new ArrayList<>();
+
+    static {
+        try (InputStream is = new ClassPathResource("words.txt").getInputStream()) {
+            Scanner sc = new Scanner(is).useDelimiter("\n"); // BEWARE - windows files have \r\n instead of just \n
+            while (sc.hasNext()) {
+                DICTIONARY.add(sc.next().toUpperCase());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String validate(String word){
+        if (word.length() != 5) {
+            return "Bad length";
+        }
+        word = word.toUpperCase();
+        if (!DICTIONARY.contains(word)){
+            return "Not in the dictionary";
+        }
+        return null;
+    }
+    public CharacterResult[] calculateResult(String word){
+        CharacterResult[] result = new CharacterResult[WORD.length()];
+        for(int i = 0; i< word.length();i++){
+            char currentChar = word.charAt(i);
+            if(currentChar == WORD.charAt(i)){
+                result[i] = CharacterResult.GREEN;
+                continue;
+            }
+            if (WORD.indexOf(currentChar) > -1){
+                result[i] = CharacterResult.YELLOW;
+                continue;
+            }
+            result[i]= CharacterResult.BLACK;
+        }
+        return result;
+    }
+}
--- src/main/java/com/adhunchanchoc/debugwordlespring/webservices/WordleWS.java
+++ src/main/java/com/adhunchanchoc/debugwordlespring/webservices/WordleWS.java
@@ -1,5 +1,6 @@
 package com.adhunchanchoc.debugwordlespring.webservices;
 
+import com.adhunchanchoc.debugwordlespring.service.WordleService;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,44 +11,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+//@AllArgsConstructor
 @RestController
 public class WordleWS {
-    private static final String WORD = "LYMPH";
-    private static final List<String> DICTIONARY = new ArrayList<>();
 
-    static {
-        try (InputStream is = new ClassPathResource("words.txt").getInputStream()) {
-            Scanner sc = new Scanner(is).useDelimiter("\n"); // BEWARE - windows files have \r\n instead of just \n
-            while(sc.hasNext()){
-                DICTIONARY.add(sc.next().toUpperCase());
-            }
-        }catch (IOException e){
-            throw new RuntimeException(e);
-        }
+    private final WordleService wordleService;
+//    this constructor can be replaced by Lombok @AllArgsConstructor
+    public WordleWS(WordleService wordleService){
+        this.wordleService = wordleService;
     }
 
     @GetMapping("/guess")
     public Result guess(String word) {
-        if (word.length() != 5) {
-            return new Result(null,"Bad length");
+        var error = wordleService.validate(word);
+        if(error != null) {
+            return new Result(null,error);
         }
         word = word.toUpperCase();
-        if (!DICTIONARY.contains(word)){
-            return new Result (null, "Not in the dictionary");
-        }
-        CharacterResult[] result = new CharacterResult[WORD.length()];
-        for(int i = 0; i< word.length();i++){
-            char currentChar = word.charAt(i);
-            if(currentChar == WORD.charAt(i)){
-                result[i] = CharacterResult.GREEN;
-                continue;
-            }
-            if (WORD.indexOf(currentChar) > -1){
-                result[i] = CharacterResult.YELLOW;
-                continue;
-            }
-            result[i]= CharacterResult.BLACK;
-        }
-        return new Result(result,null);
+        return new Result(wordleService.calculateResult(word),null);
     }
 }
